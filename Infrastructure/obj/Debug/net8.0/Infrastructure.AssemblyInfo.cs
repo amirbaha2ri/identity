30ba71b7f2a11928e66487958543aa6759a32c44
Amirabbas.Bahadori <amirabbas.bahadori@zarrino.io>
2024-08-20 17:34:45 +0330

    .

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93461416ad3eed91280ae7daa6b4dd77a345ab9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7deb32ee8d94e8c713cfc95438a8c113e77186")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WebApi/Controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApi.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class VersionController : ControllerBase
+{
+    [HttpGet(Name = "Version")]
+    [Authorize]
+    public string Get()
+    {
+        return "0.0.1";
+    }
+}
